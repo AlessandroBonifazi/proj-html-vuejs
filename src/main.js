fbd8c45261df53537f98f6d45e9469019f8e3f51
AlessandroBonifazi <alessandrobonifazi21@gmail.com>
2022-04-28 00:34:48 +0200

    added icons

--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+/* import fontawesome  */
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faLightbulb } from '@fortawesome/free-solid-svg-icons'
+import { faComment } from '@fortawesome/free-regular-svg-icons'
+import { faCloud } from '@fortawesome/free-solid-svg-icons'
+import { faPencil } from '@fortawesome/free-solid-svg-icons'
+// import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+// import { faPinterestP } from '@fortawesome/free-brands-svg-icons'
+// import { faFacebookF } from '@fortawesome/free-brands-svg-icons'
+// import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
+
+library.add(faUserSecret, faLightbulb, faComment, faCloud, faPencil)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
