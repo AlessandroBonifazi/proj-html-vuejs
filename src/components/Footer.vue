afd1627b9156887d5049450a5757fe7813f822d2
AlessandroBonifazi <alessandrobonifazi21@gmail.com>
2022-04-28 16:56:15 +0200

    added footer style

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,21 +1,25 @@
 <template>
   <div class="container-fluid ab-footer-bg mt-5">
     <div class="row ab-h300 d-flex justify-content-center align-items-center">
+      <!-- logo -->
       <div class="col-2 d-flex flex-column align-items-start">
         <img src="@/assets/img/logo-sidearea-1.png" alt="logo" />
         <span>Let's get creative</span>
       </div>
 
+      <!-- contacts -->
       <div class="col-2 d-flex flex-column align-items-start">
         <span>{{ email }}</span>
         <span>{{ phone }}</span>
       </div>
 
+      <!-- location -->
       <div class="col-2 d-flex flex-column align-items-start">
         <span>{{ address }}</span>
         <span>{{ city }}</span>
       </div>
 
+      <!-- social -->
       <div class="col-2 d-flex flex-column align-items-start">
         <span>Stay in touch with us</span>
         <div class="social w-50 mt-2 d-flex justify-content-between">
@@ -55,6 +59,13 @@ export default {
   background-color: #c0e1cf;
   .social-icon {
     font-size: 25px;
+  }
+  img {
+    height: 40px;
+  }
+  span {
+    margin: 5px;
+    font-size: 16px;
   }
 }
 </style>
