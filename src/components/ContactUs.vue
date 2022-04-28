44414f874e6d2de5902fab0c7ffc964761b13013
AlessandroBonifazi <alessandrobonifazi21@gmail.com>
2022-04-28 02:06:30 +0200

    added dom TBS

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -1,7 +1,25 @@
 <template>
-  <div class="container-fluid">
-    <div class="row">
-      <h1>ContactUsComponent</h1>
+  <div class="container-fluid my-5 ab-contact-bg">
+    <div class="row mt-5">
+      <div
+        class="
+          col-10
+          offset-1
+          d-flex
+          flex-column
+          align-items-center
+          text-center
+        "
+      >
+        <h1>Want to work with us? Send us <strong>a message!</strong></h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+          nulla dolor voluptatum delectus alias dolorem ipsa laudantium eum
+          reiciendis sunt. Similique nulla dolor voluptatum delectus alias
+          dolorem ipsa laudantium eum reiciendis sunt.
+        </p>
+        <button>CONTACT</button>
+      </div>
     </div>
   </div>
 </template>
@@ -13,4 +31,15 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.ab-contact-bg {
+  background-image: url("@/assets/img/h-2-background-1.jpg");
+  padding: 80px;
+  h1 {
+    font-size: 80px;
+    margin-bottom: 30px;
+  }
+  p {
+    font-size: 35px;
+  }
+}
 </style>
